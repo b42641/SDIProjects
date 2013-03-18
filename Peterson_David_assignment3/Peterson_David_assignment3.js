47b7f27ee1c0e882cfefce75b5dae5c3be843f4a
David Peterson <dbpete@yahoo.com>
2013-03-18 14:42:35 -0400

    Added first files to repo

---
+++ Peterson_David_assignment3/Peterson_David_assignment3.js
@@ -0,0 +1,38 @@
+
+
+
+
+var json = {
+    "46Peaks": {
+        "Marcy": {
+        "peakHeight": 5344,
+        "peakDifficulty": 9,
+        "peakView": 10,
+        "distance": 14.8,
+        }
+        "Algonquin": {
+        "peakHeight": 5114,
+        "peakDifficulty": 7,
+        "peakView": 10,
+        "distance": 9.6,
+        }
+        "Haystack": {
+        "peakHeight": 4960,
+        "peakDifficulty": 10,
+        "peakView": 9,
+        "distance": 17.8,
+        }
+        "Skylight": {
+        "peakHeight": 4926,
+        "peakDifficulty": 10,
+        "peakView": 9,
+        "distance": 17.9,
+        }
+        "Whiteface": {
+        "peakHeight": 4867,
+        "peakDifficulty": 8,
+        "peakView": 6,
+        "distance": 10.4,
+        }
+    }
+}
