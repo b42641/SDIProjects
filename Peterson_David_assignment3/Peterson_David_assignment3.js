516d1a6dd8d67e60d1d94068ba7740546833ea94
David Peterson <dbpete@yahoo.com>
2013-03-19 21:14:22 -0400

    first real work on project

--- Peterson_David_assignment3/Peterson_David_assignment3.js
+++ Peterson_David_assignment3/Peterson_David_assignment3.js
@@ -3,14 +3,44 @@
 // SID 1303 assignment 3
 
 
+var peak = {
+    peakName: "Marcy",
+    peakHeight: 5344,
+    peakTrail: true,
+    peakTrailhead: ["St. Johns", "Upper Works", "Elk Lake"],
+    hikeTo: function(trailHead) {},
+    chooseTrailhead: function () {
+        console.log("The following are trailheads to the peak: ")
+        for (var i = 0; i < peak.peakTrailhead.length; i++) {
+            var potentialTrails = peak.peakTrailhead[i];
+            console.log(potentialTrails);
+        } //for
+    } //chooseTrailhead
+}; //var
+
+// List trailhead options by displaying each potential trailhead
+peak.chooseTrailhead ();
+
+
+
+
+
+
+
+
+
+
+
 // Climb the Highest Peaks in the Adirondacks
 
-var handleData = function (json) {
-    
-};
-handleData(json);
+for (var key in json.highPeaks) {
+    var peakName = json.highPeaks[key];
+    console.log(key);
+    console.log(peakName);
+}
 
-console.log(json);
+
+
 
 /*    "46Peaks": [
         {
